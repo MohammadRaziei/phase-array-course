998a2c84c51b80951dcad3752258a0c2c45f520c
MohammadRaziei <mohammadraziei1375@gmail.com>
2024-12-04 08:56:21 +0330

    HW01 complete

--- Q6/run_for_P.m
+++ Q6/run_for_P.m
@@ -51,7 +51,7 @@ af_phi_0 = af(Phi==0);
 
 
 
-figure('units','normalized','position',[0 .25 1 .5])
+figure('units','normalized','position',[0 .25 1 .5]); hold on
 ax = gca;
 outerpos = ax.OuterPosition;
 ti = ax.TightInset; 
@@ -70,6 +70,10 @@ xticklabels(arrayfun(@(s) sprintf("%g \\pi", s), (-1:0.25:1),'UniformOutput',tru
 ylim([-5, 30]);
 
 
+plot(theta_phi_0(MaxIdx), af_phi_0(MaxIdx), ".b");
+plot(theta_phi_0(MinIdx), af_phi_0(MinIdx), ".b");
+
+
 for i = MaxIdx'
     text(theta_phi_0(i), af_phi_0(i)+1.8, sprintf("\\lceil%.2g\\pi", round(theta_phi_0(i)/pi, 2)), "HorizontalAlignment", "left", "Color", .4*ones(1,3))
     text(theta_phi_0(i), af_phi_0(i)+.8, sprintf("\\lfloor%.2g", round(af_phi_0(i),2)), "HorizontalAlignment", "left", "Color", .4*ones(1,3))
@@ -79,12 +83,13 @@ for i = MinIdx'
     text(theta_phi_0(i), af_phi_0(i)-1.8, sprintf("\\lfloor%.2g", round(af_phi_0(i),2)), "HorizontalAlignment", "left", "Color", .4*ones(1,3))
 end
 
+
 exportgraphics(gcf, sprintf('results/%s-phi-0-mag.pdf', prefix), 'Append', false);
 
 
 
 
-figure('units','normalized','position',[0 .25 1 .5])
+figure('units','normalized','position',[0 .25 1 .5]); hold on
 ax = gca;
 outerpos = ax.OuterPosition;
 ti = ax.TightInset; 
@@ -99,11 +104,33 @@ af_phi_0_db = mag2db(af_phi_0);
 
 plot(theta_phi_0, af_phi_0_db)
 
+% ll = length(af_phi_0_db);
+% is = floor(ll/4); ie = ceil(ll*3/4);
+b = find(af_phi_0_db >= (max(af_phi_0_db) - 3));
+a = diff([0; b]);
+a = find((circshift(a,-1)>1) + (a>1));
+if b(1) == 1, a = a(2:end-1); end 
+idx_hpbw = reshape(b(a), 2, []); % 2 * n; each col each pair; 
+
 xlim([-1, 1]*pi); xticks((-1:0.25:1)*pi)
 xticklabels(arrayfun(@(s) sprintf("%g \\pi", s), (-1:0.25:1),'UniformOutput',true))
 xlabel("\theta"); ylabel("dB")
 ylim([-50, 40]);
 
+plot(theta_phi_0(idx_hpbw(:)), af_phi_0_db(idx_hpbw(:)), "or");
+yline((max(af_phi_0_db) - 3))
+
+for i = 1:size(idx_hpbw, 2)
+    text(theta_phi_0(idx_hpbw(1,i))-.2, af_phi_0_db(idx_hpbw(1,i))+2.5+2.5, sprintf("\\lceil%.3g\\pi", round(theta_phi_0(idx_hpbw(1,i))/pi, 3)), "HorizontalAlignment", "left", "Color", "red")
+    text(theta_phi_0(idx_hpbw(1,i))-.2, af_phi_0_db(idx_hpbw(1,i))+2.5, sprintf("\\lfloor%.3g", round(af_phi_0_db(idx_hpbw(1,i))/pi, 3)), "HorizontalAlignment", "left", "Color", "red")
+    text(theta_phi_0(idx_hpbw(2,i))+.02, af_phi_0_db(idx_hpbw(2,i))+2.5+2.5, sprintf("\\lceil%.3g\\pi", round(theta_phi_0(idx_hpbw(2,i))/pi, 3)), "HorizontalAlignment", "left", "Color", "red")
+    text(theta_phi_0(idx_hpbw(2,i))+.02, af_phi_0_db(idx_hpbw(2,i))+2.5, sprintf("\\lfloor%.3g", round(af_phi_0_db(idx_hpbw(2,i))/pi, 3)), "HorizontalAlignment", "left", "Color", "red")
+
+    text(mean(theta_phi_0(idx_hpbw(:,i))), mean(af_phi_0_db(idx_hpbw(:,i)))+12, sprintf("HPBW = %.3f\\pi", diff(theta_phi_0(idx_hpbw(:,i))/pi)), "HorizontalAlignment", "center", "Color", "blue")
+end
+
+plot(theta_phi_0(MaxIdx), af_phi_0_db(MaxIdx), ".b");
+plot(theta_phi_0(MinIdx), af_phi_0_db(MinIdx), ".b");
 
 for i = MaxIdx'
     text(theta_phi_0(i), af_phi_0_db(i)+2.5+2.5, sprintf("\\lceil%.2g\\pi", round(theta_phi_0(i)/pi, 2)), "HorizontalAlignment", "left", "Color", .4*ones(1,3))
